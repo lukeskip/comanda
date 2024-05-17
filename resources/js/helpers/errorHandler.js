06338122d06564a3124df4dc42591041324736e8
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-05-17 10:44:11 -0600

    error handler with sweetAlert

---
+++ resources/js/helpers/errorHandler.js
@@ -0,0 +1,19 @@
+import Swal from 'sweetalert2'
+import { router } from '@inertiajs/vue3';
+import {useStore} from 'vuex';
+
+const errorHandler = (error,store = ()=>{} )=>{
+   
+    if(error.response.status !== 422){  
+       
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: error.message,
+            footer: 'Por favor inténtalo de nuevo'
+        }).then(()=>{
+            store.commit('toggleLoader');
+        });
+    }
+}
+export default errorHandler;
